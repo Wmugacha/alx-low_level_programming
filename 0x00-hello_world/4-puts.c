b0a002b691610d037f9aa4509e8ddbb43e864d4c
Wmugacha <wilfredmugacha@gmail.com>
2022-09-08 21:11:47 +0000

    Task 4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * This is a simple program to print out s string
+ */
+
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
+
