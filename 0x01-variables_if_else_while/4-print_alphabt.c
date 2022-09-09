9d59ca83cd328ab31a378790703db047ee7d8ae1
Wmugacha <wilfredmugacha@gmail.com>
2022-09-09 23:10:42 +0000

    Task 4. When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	char abz[24] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		putchar(abz[i]);
+	}
+	putchar('\n');
+	return (0);
+}
