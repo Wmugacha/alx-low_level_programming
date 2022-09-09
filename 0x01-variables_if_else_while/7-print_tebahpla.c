beea98ccdfaccdeea74887ebded5f462b0a32b5b
Wmugacha <wilfredmugacha@gmail.com>
2022-09-09 23:56:57 +0000

    Task 7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	char abz[26] = "zyxwvutsrqponmlkjihgfedcba";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(abz[i]);
+	}
+	putchar('\n');
+	return (0);
+}
