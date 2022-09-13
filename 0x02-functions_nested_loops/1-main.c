c5f9c78b9f9f25dda2617732633b3db0d5e06028
Wmugacha <wilfredmugacha@gmail.com>
2022-09-13 15:55:11 +0000

    Task 1 commit

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Returns: on success 1
+ *
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define _ALX_S_
 
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif
