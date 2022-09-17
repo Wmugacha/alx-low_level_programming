e4b7786c982cfbf5f1815b5266705c79159911f4
Wmugacha <wilfredmugacha@gmail.com>
2022-09-17 00:03:58 +0000

    Task 9 - FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int num;
 
-	for (num = 1; num <= 100; num++)
+	for (num = 1; num < 100; num++)
 	{
 		if (num % 3 == 0 && num % 5 == 0)
 			printf("FizzBuzz ");
@@ -21,6 +21,6 @@ int main(void)
 		else
 			printf("%d ", num);
 	}
-	printf("\n");
+	printf("Buzz\n");
 	return (0);
 }
