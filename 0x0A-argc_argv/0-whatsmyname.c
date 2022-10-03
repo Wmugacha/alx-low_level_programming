4149a03c8cc874b81cfb31a19f0759e63eba1be0
Wmugacha <wilfredmugacha@gmail.com>
2022-10-03 04:45:04 +0000

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints its name, followed by a new line.
+ * @argc: size of argv array.
+ * @argv: array of strings.
+ * Return: Always 0.
+ */
+
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return 0;
+}
