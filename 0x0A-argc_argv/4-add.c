a5ffb6d441ed47be596962ef0e0aed2aab6fd18a
Wmugacha <wilfredmugacha@gmail.com>
2022-10-03 11:09:12 +0000

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - funtion of a program that gets the sum of numbers.
+ * @argc: size of argv[] array.
+ * @argv: string array.
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
