f7a13859eaa8b5154f31279b5405d396facf366e
Wmugacha <wilfredmugacha@gmail.com>
2022-10-11 17:15:32 +0000

    Finished tasks

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef _ABS_H_
+#define _ABS_H_
+
+/**
+ * ABS - computes absolute value of a number x.
+ * @x: number to be computed.
+ */
+
+#define ABS(x) ((x < 0) ? (-x) : (x))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef _SUM_H_
+#define _SUM_H_
+
+/**
+ * SUM - macro that computes sum of x and y.
+ * @x: first number.
+ * @y: second number.
+ */
+
+#define SUM(x, y)((x) + (y))
+
+
+#endif /*_SUM_H_*/
