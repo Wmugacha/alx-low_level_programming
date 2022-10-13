9a0a5dd1178159030995e5e2c6eb3fe59d7c3f81
Wmugacha <wilfredmugacha@gmail.com>
2022-10-13 14:52:24 +0000

    initial commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_name - funtion to print a name.
+ * @name: name to be printed
+ * @f: funtion pointer.
+ * Return: Always 0.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
