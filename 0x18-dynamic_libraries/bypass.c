51866f41e874dafde13cd5e15428bc7f00006f64
Wmugacha <wilfredmugacha@gmail.com>
2022-12-17 20:56:39 +0000

    please make me win

--- 0x18-dynamic_libraries/bypass.c
+++ 0x18-dynamic_libraries/bypass.c
@@ -7,16 +7,16 @@ int rand()
 
 	nm++;
 	if (nm == 0)
-		return 9;
+		return 8;
 	if (nm == 1)
 		return 8;
 	if (nm == 2)
-		return 10;
+		return 7;
 	if (nm == 3)
-		return 24;
-	if (nm == 4)
-		return 75;
-	if (nm == 5)
 		return 9;
+	if (nm == 4)
+		return 23;
+	if (nm == 5)
+		return 74;
 	return nm * nm % 30000;
 }
